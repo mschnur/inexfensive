6e1c3cff75010eac97f2146bc164955da5f3c61a
mschnur <matthew.p.schnur@gmail.com>
2016-02-17 15:08:42 -0500

    Look for minimum foil depress time instead of median

--- FencingBox/FencingConstants.h
+++ FencingBox/FencingConstants.h
@@ -4,7 +4,7 @@
 #define EPEE_DEPRESS_US 2000 // (2ms - 10ms)
 #define EPEE_LOCKOUT_US 45000 // (45ms +/- 5ms)
 
-#define FOIL_DEPRESS_US 14000 // (14ms +/- 1ms)
+#define FOIL_DEPRESS_US 13000 // (14ms +/- 1ms)
 #define FOIL_LOCKOUT_US 300000 // (300ms +/- 25ms)
 
 #define SABER_DEPRESS_US 100 // (0.1ms - 1ms)

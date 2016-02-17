7b31b704e8d0b4943113e7cb353c0f2498f6e5d5
mschnur <matthew.p.schnur@gmail.com>
2016-02-17 15:07:39 -0500

    Add support for new weapon mode change handling, also added code to change ADC prescaler to 32 for faster analogReads

--- FencingBox/FencingBox.ino
+++ FencingBox/FencingBox.ino
@@ -1,11 +1,12 @@
 
 /********************************* Includes ***********************************/
 /*
- * The SPI library needs to be included here because for a library's header to be
+ * These libraries need to be included here because for a library's header to be
  * included in the header search path in *other* files, it must be included in the
  * main sketch file (this file).
  */
 #include <SPI.h>
+#include <TimerOne.h>
 
 #include "DetectionInterface.h"
 #include "DisplayInterface.h"
@@ -30,12 +31,39 @@ WeaponMode * saberMode;
 Fencer fencerA = Fencer(FENCER_A);
 Fencer fencerB = Fencer(FENCER_B);
 
-/******************************************************************************/
+/********************************* Defines ************************************/
+#define FASTADC 1
+
+// defines for setting and clearing register bits
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
 
 /******************************************************************************/
 
 void setup()
 {
+  /*
+   * Based on discussion here: http://forum.arduino.cc/index.php?topic=6549.0,
+   * as well as data here: https://github.com/greiman/SdFat-beta/blob/master/AnalogBinLoggerExtras/AdcErrorStudy.txt,
+   * and this graph: https://github.com/greiman/SdFat-beta/blob/master/AnalogBinLoggerExtras/ADC_ENOB.PNG,
+   * It seems that the ADC prescaler can be changed to decrease the time required for each analogRead, at
+   * the cost of some accuracy on the reads.  It appears from the above sources that this loss of accuracy
+   * is very minor if the prescaler is not decreased too much.  As such, I am decreasing the prescaler
+   * by two levels, from the default 128 to 32.  I referenced Table 24-5 from page 250 of the ATmega328P 
+   * (the chip in the Arduino Uno) datasheet, which can be found here:
+   * http://www.atmel.com/images/atmel-8271-8-bit-avr-microcontroller-atmega48a-48pa-88a-88pa-168a-168pa-328-328p_datasheet_complete.pdf
+   */
+#if FASTADC
+  // set prescale to 32
+  sbi(ADCSRA, ADPS2) ;
+  cbi(ADCSRA, ADPS1) ;
+  sbi(ADCSRA, ADPS0) ;
+#endif
+
   // init weapon modes
   epeeMode = new EpeeMode();
   foilMode = new FoilMode();
@@ -51,6 +79,8 @@ void setup()
   setup_detection(currentWeaponMode->getType());
   setup_display();
   setupWeaponChange();
+
+//  lightTest();
 }
 
 void loop()
@@ -59,15 +89,15 @@ void loop()
   // standard loop mechanism (i.e. the loop() method being called repeatedly)
   while (1)
   {
-    // switchWeaponMode will be set to true in an interrupt when
+    // The switchWeaponModeCount will be incremented in an interrupt when
     // the change weapon button is pressed on the box or remote
-    if (switchWeaponMode())
+    if (getSwitchWeaponModeCount() > 0)
     {
       switchWeapons();
     }
 
-    // if there is time remaining in the match
-    if (timeIsLeft())
+    // if there is time remaining in the match and we are not in the process of diplaying a touch
+    if (timeIsLeft() && !displayingTouch)
     {
       // detect state changes during this iteration
       currentWeaponMode->updateStatus(fencerA, fencerB, locked_out);
@@ -80,25 +110,23 @@ void loop()
 
 void display_signals()
 {
-  // if we are locked out than an an on target or off target touch has occurred
-  if (locked_out)
-  {
-    signalTouch(fencerA.touch,
-                 fencerA.off_target,
-                 fencerB.touch,
-                 fencerB.off_target);
-
-    fencerA.self_contact_changed = false;
-    fencerB.self_contact_changed = false;
-    reset();
-  }
-  
   if (fencerA.self_contact_changed || fencerB.self_contact_changed)
   {
     updateSelfContact(fencerA.self_contact, fencerB.self_contact);
 
     fencerA.self_contact_changed = false;
     fencerB.self_contact_changed = false;
+  }
+  
+  // if we are locked out than an an on target or off target touch has occurred
+  if (locked_out)
+  {
+    signalTouch(fencerA.touch,
+                fencerA.off_target,
+                fencerB.touch,
+                fencerB.off_target);
+                
+    reset();
   }
 }
 
@@ -113,31 +141,35 @@ void reset()
 
 void switchWeapons()
 {
-  // reset the flag
-  weaponModeChanged();
-
-  // set the current weapon mode (cycle through modes)
-  switch (currentWeaponMode->nextWeaponType())
+  while (getSwitchWeaponModeCount() > 0)
   {
-    case EPEE:
-      currentWeaponMode = epeeMode;
-      break;
+    // set the current weapon mode (cycle through modes)
+    WeaponType newType = currentWeaponMode->nextWeaponType();
+    switch (newType)
+    {
+      case EPEE:
+        currentWeaponMode = epeeMode;
+        break;
 
-    case FOIL:
-      currentWeaponMode = foilMode;
-      break;
+      case FOIL:
+        currentWeaponMode = foilMode;
+        break;
 
-    case SABER:
-      currentWeaponMode = saberMode;
-      break;
+      case SABER:
+        currentWeaponMode = saberMode;
+        break;
 
-    default:
-      // do nothing
-      break;
+      default:
+        // do nothing
+        break;
+    }
+
+    // set up detection for the newly selected weapon mode
+    setupNewWeapon(newType);
+
+    // reset the flag
+    weaponModeChanged();
   }
-
-  // set up detection for the newly selected weapon mode
-  setup_detection(currentWeaponMode->getType());
 }
 
 

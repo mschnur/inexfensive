0e9205db5df44a37d6e146be9ac5529ea60d95b0
mschnur <matthew.p.schnur@gmail.com>
2016-02-29 14:57:14 -0500

    Update pin mapping and change weapon change handling to try and protect against unintended weapon mode changes

--- FencingBox/ArduinoWeaponChangeInterface.c
+++ FencingBox/ArduinoWeaponChangeInterface.c
@@ -1,9 +1,12 @@
 
 /********************************* Includes ***********************************/
 #include "WeaponChangeInterface.h"
+#include <digitalWriteFast.h>
 
 /*********************************** Pins *************************************/
-const uint8_t pin_changeWeapon = 2;
+const uint8_t pin_changeWeaponInterrupt = 2;
+// this is signed just so I don't get a warning caused by digitalReadFast
+const int8_t pin_changeWeaponConfirmation = 4;
 
 /***************************** Internal Status ********************************/
 volatile uint8_t switchCount;
@@ -12,7 +15,10 @@ volatile uint8_t switchCount;
 
 void weaponChangeISR()
 {
-  ++switchCount;
+  if (digitalReadFast(pin_changeWeaponConfirmation))
+  {
+    ++switchCount;
+  }
 }
 
 void setupWeaponChange()
@@ -20,11 +26,13 @@ void setupWeaponChange()
   // initialize flag
   switchCount = 0;
 
-  // setup pin on which interrupt signal will be received
-  pinMode(pin_changeWeapon, INPUT);
+  // setup pin on which interrupt signal will be received as well as the confirmation pin (used
+  // to try and rule out any unexpected interrupts).
+  pinMode(pin_changeWeaponInterrupt, INPUT);
+  pinMode(pin_changeWeaponConfirmation, INPUT);
 
   // set up interrupt for changing weapon mode
-  attachInterrupt(digitalPinToInterrupt(pin_changeWeapon), weaponChangeISR, RISING);
+  attachInterrupt(digitalPinToInterrupt(pin_changeWeaponInterrupt), weaponChangeISR, RISING);
 }
 
 uint8_t getSwitchWeaponModeCount()
